576d9b1adbfbd5e992c6dd1fd7f762dcf3533d7a
Keshvi Patel <160455891+PatelKeshvi@users.noreply.github.com>
2024-07-29 20:10:43 +0530

    Final Project

---
+++ Final Project/Components/nav.js
@@ -0,0 +1,30 @@
+const nav = () => {
+    return ` <div class="nav-wrapper">
+        <div class="logo-container">
+            <img class="logo"
+                src=""
+                alt="Logo">
+        </div>
+        <nav>
+            <input class="hidden" type="checkbox" id="menuToggle">
+            <label class="menu-btn" for="menuToggle">
+                <div class="menu"></div>
+                <div class="menu"></div>
+                <div class="menu"></div>
+            </label>
+            <div class="nav-container">
+                <div class="nav-tabs">
+                    <a class="nav-tab" href="index.html">Home</a>
+                    <a class="nav-tab" href="/pages/Dashboard.html">Dashboard</a>
+                    <a class="nav-tab" href="/pages/add.html">Add Products</a>
+                    <a class="nav-tab" href="/pages/cart.html">Cart</a>
+                    <a class="nav-tab" href="/pages/signup.html">SignUp</a>
+                    <a class="nav-tab" href="/pages/login.html">LogIn</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+ `
+}
+
+export default nav
